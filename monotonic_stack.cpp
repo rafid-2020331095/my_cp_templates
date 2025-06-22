c70d909b9ff7a65513181f7c26de58ad6913713c
rafid-2020331095 <106920440+rafid-2020331095@users.noreply.github.com>
2025-06-22 09:10:18 +0600

    Update monotonic_stack.cpp

--- monotonic_stack.cpp
+++ monotonic_stack.cpp
@@ -31,3 +31,28 @@ vector<int> nextSmallerElement(vector<int>& nums) {
     }
     return result;
 }
+
+
+// nse and pse in 1 loop
+    int largestRectangleArea(vector<int>& heights,int &maxi) {
+        int n=heights.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[i]<=heights[st.top()]){
+                int element=st.top();st.pop();
+                int nse=i;
+                int pse=st.empty()?-1:st.top();
+                int area=heights[element]*(nse-pse-1);
+                maxi=max(maxi,area);
+            }
+                st.push(i);
+        }
+        while(!st.empty()){
+            int element=st.top();st.pop();
+            int nse=n;
+            int pse=st.empty()?-1:st.top();
+            int area=heights[element]*(nse-pse-1);
+            maxi=max(maxi,area);
+        }
+        return maxi;
+    }
